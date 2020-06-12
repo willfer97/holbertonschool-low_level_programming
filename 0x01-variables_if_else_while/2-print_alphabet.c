2c1d83eb7fd89cfbb935dbab211b10b2583955f4
willfer97a <wbarbosaroj1997@gmail.com>
2020-06-12 00:09:46 +0000

    Print alphabet 1.0

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char az;
+
+	for (az = 'a'; az <= 'z'; az++)
+	{
+		putchar(az);
+	}
+	putchar('\n');
+	return (0);
+}
