6494b8898fa64637812d5b37befd7a449595912d
willfer97a <wbarbosaroj1997@gmail.com>
2020-06-12 01:15:57 +0000

    PRINT ALPHABT

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char az;
+
+	for (az = 'a'; az <= 'z'; az++)
+	{
+		if (az != 'e' && az != 'q')
+		{
+			putchar(az);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
