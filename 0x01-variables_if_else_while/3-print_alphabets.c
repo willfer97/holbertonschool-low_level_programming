6e101651c4a3f4d5b19402a483290ccdb2a1b4e5
willfer97a <wbarbosaroj1997@gmail.com>
2020-06-12 00:30:26 +0000

    Print ALPHAbets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char az;
+	char AZ;
+
+	for (az = 'a'; az <= 'z'; az++)
+	{
+		putchar(az);
+	}
+	for (AZ = 'A'; AZ <= 'Z'; AZ++)
+	{
+		putchar(AZ);
+	}
+	putchar('\n');
+	return (0);
+}
