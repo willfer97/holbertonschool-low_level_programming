b947523c66d37aa4c65a3a148f0a5923ea6dfdc9
willfer97a <wbarbosaroj1997@gmail.com>
2020-06-12 01:50:19 +0000

    Z..A

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char za;
+
+	for (za = 'z'; za >= 'a'; za--)
+	{
+		putchar(za);
+	}
+	putchar('\n');
+	return (0);
+}
