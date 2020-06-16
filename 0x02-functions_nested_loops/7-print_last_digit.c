42204ce488eeddff556212b9a7361349d0b6d676
willfer97a <wbarbosaroj1997@gmail.com>
2020-06-16 22:42:16 +0000

    New update to fix print las digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,8 +6,10 @@
  */
 int print_last_digit(int n)
 {
+	int digit;
+	digit = n % 10;
 	if (n < 0)
-		n = n * -1;
-	_putchar((n % 10) + '0');
-	return (n % 10);
+		digit = -digit;
+	_putchar(digit + '0');
+	return (digit);
 }
