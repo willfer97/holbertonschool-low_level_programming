6d1c6770f3aa431b05ed3ad8656e8f2cf06badd1
willfer97a <wbarbosaroj1997@gmail.com>
2020-06-16 12:59:10 +0000

    new update to fix alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,5 +1,9 @@
 #include "holberton.h"
-
+/**
+ * _isalpha - check the words if is alpha.
+ * @c: character to test
+ * Return: 0 if is true or 1 else isnt true.
+ */
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
