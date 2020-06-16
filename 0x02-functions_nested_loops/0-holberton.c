c4416eaca985cf157b10f41579db176d26b20edc
willfer97a <wbarbosaroj1997@gmail.com>
2020-06-16 10:47:59 +0000

    update to fix error 1.1

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,15 +7,14 @@
  */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
+	char c[] = "Holberton\0";
+	int i;
+
+	while (c[i] != '\0')
+	{
+		_putchar(c[i]);
+		i++;
+	}
 	_putchar('\n');
 	return (0);
 }
