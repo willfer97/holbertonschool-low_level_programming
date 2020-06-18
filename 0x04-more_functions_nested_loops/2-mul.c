15d518048178a91277ba0e182ff1306c035b369c
willfer97a <wbarbosaroj1997@gmail.com>
2020-06-18 15:45:31 +0000

    Fucntion tu multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * mul - Function to multiplies two integers
+ * @a: first variable used to function
+ * @b: second variable used to function
+ * Return: return value to multiplied two integers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
