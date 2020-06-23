878f6eccb6115250e00688ef553dfc88f2dbc3f6
willfer97a <wbarbosaroj1997@gmail.com>
2020-06-23 19:03:55 +0000

    new update to 5 task

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * rev_string - Function to reverse string
+ * @s: imput to reverse string∑∫
+ */
+void rev_string(char *s)
+{
+	int i, j;
+	char r[10];
+
+	i = j = 0;
+	while (s[i] != '\0')
+	{
+		r[i] = s[i];
+		i++;
+	}
+	i--;
+	while (i >= 0)
+	{
+		s[i] = r[j];
+		i--;
+		j++;
+	}
+}
