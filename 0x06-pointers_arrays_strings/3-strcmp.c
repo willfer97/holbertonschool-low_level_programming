29bd52a2318dbee37921907fd7c6fa397936d8ab
willfer97a <wbarbosaroj1997@gmail.com>
2020-06-25 23:21:55 +0000

    New update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,13 +7,11 @@
  **/
 int _strcmp(char *s1, char *s2)
 {
-	if (s1 < s2)
-	{
-		return (-15);
-	}
-	else if (s1 > s2)
-	{
-		return (15);
-	}
-	return (0);
+	int x;
+
+	x = 0;
+
+	while (s1[x] && s2[x] && s1[x] == s2[x])
+		x++;
+	return (s1[x] - s2[x]);
 }
