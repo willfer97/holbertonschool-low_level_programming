ebaff4f2ab7c9ec40cc724aeaadcb560158eef62
willfer97a <wbarbosaroj1997@gmail.com>
2020-06-26 00:03:33 +0000

    lowercase to uppercase 1.0

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * string_toupper - Lowercase to Uppercase
+ * @c: Add variable char type
+ * Return: Value c
+ */
+char *string_toupper(char *c)
+{
+	int i = 0;
+
+	while (c[i])
+	{
+		i++;
+		if (c[i] > 'a' && c[i] < 'z')
+			c[i] -= 32;
+	}
+	return (c);
+}
